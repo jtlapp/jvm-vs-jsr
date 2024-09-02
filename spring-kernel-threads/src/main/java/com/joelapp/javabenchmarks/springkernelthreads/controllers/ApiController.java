60f836fc1342979ec67a08ac25885864bc2bc9e6
Joseph T. Lapp <arachnojoe@gmail.com>
2024-09-01 22:40:22 -0400

    Renamed TestController to ApiController

--- spring-kernel-threads/src/main/java/com/joelapp/javabenchmarks/springkernelthreads/controllers/ApiController.java
+++ spring-kernel-threads/src/main/java/com/joelapp/javabenchmarks/springkernelthreads/controllers/ApiController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
-public class TestController {
+public class ApiController {
 
     @Autowired
     private HikariQueries queries;
