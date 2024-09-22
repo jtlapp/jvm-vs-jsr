99b7552c84e65168a5c1013d3e85a370bec1e134
Joseph T. Lapp <arachnojoe@gmail.com>
2024-09-22 19:17:30 -0400

    Added jooby-nomvc-jdbc-app

--- jooby-nomvc-jdbc-app/src/main/java/app/JoobyNoMVCJdbcApp.java
+++ jooby-nomvc-jdbc-app/src/main/java/app/JoobyNoMVCJdbcApp.java
@@ -1,17 +1,40 @@
 package app;
 
+import com.joelapp.javabenchmarks.sharedquery.SharedQueryDB;
+import com.joelapp.javabenchmarks.sharedquery.SharedQuery;
+import com.joelapp.javabenchmarks.sharedquery.SharedQueryException;
+import com.joelapp.javabenchmarks.sharedquery.SharedQueryRepo;
 import io.jooby.Jooby;
+import io.jooby.exception.BadRequestException;
 import io.jooby.netty.NettyServer;
 
-public class App extends Jooby {
+public class JoobyNoMVCJdbcApp extends Jooby {
 
   {
     install(new NettyServer());
-    get("/", ctx -> "Welcome to Jooby!");
+
+    SharedQueryDB sharedQueryDB = new SharedQueryDB(
+            System.getenv("DATABASE_URL"),
+            System.getenv("DATABASE_USERNAME"),
+            System.getenv("DATABASE_PASSWORD"));
+    SharedQueryRepo sharedQueryRepo = new SharedQueryRepo();
+
+    get("/", ctx -> "Running Jooby without MVC, with JDBC\n");
+
+    post("/api/query/{queryName}", ctx -> {
+      String queryName = ctx.path("queryName").value();
+      String jsonBody = ctx.body().value();
+      try {
+        SharedQuery query = sharedQueryRepo.get(sharedQueryDB, queryName);
+        String jsonResponse = query.executeUsingGson(sharedQueryDB, jsonBody);
+        return ctx.send(jsonResponse);
+      } catch (SharedQueryException e) {
+        throw new BadRequestException(String.format("{\"error\": \"%s\"}", e.getMessage()));
+      }
+    });
   }
 
   public static void main(final String[] args) {
-    runApp(args, App::new);
+    runApp(args, JoobyNoMVCJdbcApp::new);
   }
-
 }
