2e6084d1d7890f075b38398c05cc945e5a429401
Joseph T. Lapp <arachnojoe@gmail.com>
2024-11-30 12:52:00 -0500

    Put spring-webflux-app ServerConfig in AppConfig

--- app/spring-webflux-app/src/main/java/com/jtlapp/jvmvsjs/springwebflux/config/AppConfig.java
+++ app/spring-webflux-app/src/main/java/com/jtlapp/jvmvsjs/springwebflux/config/AppConfig.java
@@ -2,6 +2,7 @@ package com.jtlapp.jvmvsjs.springwebflux.config;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jtlapp.jvmvsjs.javalib.CommonServerConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -15,5 +16,14 @@ public class AppConfig {
 
     public JsonNode toJsonNode(ObjectMapper mapper) {
         return mapper.valueToTree(this);
+    }
+}
+
+// Netty doesn't appear to be much configurable within Spring Boot.
+
+@Component
+class ServerConfig extends CommonServerConfig {
+    ServerConfig() {
+        super("Netty");
     }
 }
