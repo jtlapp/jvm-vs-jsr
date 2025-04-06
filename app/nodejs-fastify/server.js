9d9d7b52ac58f25618cf601539cc8bf6079b6803
Joseph T. Lapp <arachnojoe@gmail.com>
2025-04-06 14:44:19 -0400

    Got nodejs-fastify working

--- app/nodejs-fastify/server.js
+++ app/nodejs-fastify/server.js
@@ -13,7 +13,7 @@ const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 const pool = new Pool({
   host: process.env.DATABASE_HOST_NAME,
   database: process.env.DATABASE_NAME,
-  user: process.env.DATABASE_USER,
+  user: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   port: process.env.DATABASE_PORT,
 });
